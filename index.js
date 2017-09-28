f448ae9a0fa65f031e0ba75f081e043f4549fafd
debjohnson33 <debjohnson_11@yahoo.com>
2017-09-27 21:45:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
-	drivers.push("Ralph");
+	drivers.push(name);
 }
 
 function destructivelyPrependDriver(name) {
-	drivers.unshift("Bob");
+	drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver() {
