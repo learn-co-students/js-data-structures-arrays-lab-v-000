c26a3c9f51292474fac03666304cf7b7814d43d9
github username pianistps <pianistps@yahoo.com>
2017-10-04 14:10:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+}
+
+function appendDriver(name) {
+  const newDrivers = [...drivers, name]
+  return newDrivers
+}
+
+function prependDriver(name) {
+  const prependNew = [name, ...drivers]
+  return prependNew
+}
+
+function removeLastDriver(){
+  return drivers.slice(0, drivers.length - 1)
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1)
+}
