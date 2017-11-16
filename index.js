69ec36ea71a984005f9adb32aeea33c087540fef
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-11-15 21:48:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver (name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver (name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver (name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver (name) {
+  drivers.shift(name);
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+function prependDriver (name) {
+  return [name, ...drivers];
+}
