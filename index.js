f3b40d09c8fe1a53c888e6effab282a281333b7e
github username <github email address>
2017-12-12 14:07:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  return drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver() {
+  return drivers.shift(name);
+}
+
+// interesting
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers]
+}
+
+//slice is NOT destructive ---> also will just return what you are not slicing off, where 2nd arg is how many
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
