574564e826e5bb8277aa8476acc57e595859431a
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 14:51:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(driver) {
+  drivers.push(driver)
+}
+
+function destructivelyPrependDriver(driver) {
+  drivers.unshift(driver)
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift()
+}
+
+function appendDriver(driver) {
+  copy = [...drivers, driver]
+  return copy
+}
+
+function prependDriver(driver) {
+  copy = [driver, ...drivers]
+  return copy
+}
+
+function removeLastDriver() {
+  copy = drivers.slice(0, drivers.length - 1)
+  return copy
+}
+
+function removeFirstDriver() {
+  copy = drivers.slice(1, drivers.length)
+  return copy
+}
