9735c9d05b1a23a6d6188b2dc07f2f224b14ddd5
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-30 10:52:37 -0400

    Practice simple array manipulation

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const allDrivers = [...drivers, "Broom"];
+  return allDrivers;
+}
+
+function prependDriver(name) {
+  const allDrivers = ["Arnold", ...drivers];
+  return allDrivers;
+}
+
+function removeLastDriver() {
+  const updatedDrivers = drivers.slice(0, 2);
+  return updatedDrivers;
+}
+
+function removeFirstDriver() {
+  const newDrivers = drivers.slice(1);
+  return newDrivers;
+}
