6a0d608730292e150f5c6f024fd6a977757fa932
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-21 00:17:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const updatedDrivers = [...drivers, name];
+  
+  return updatedDrivers
+}
+
+function prependDriver(name) {
+  const updatedDrivers = [name, ...drivers]
+  
+  return updatedDrivers
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, -1)
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1)
+}
