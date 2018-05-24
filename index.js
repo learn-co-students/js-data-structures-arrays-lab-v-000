fc3207b70e91fa9db93d08c2f6977616403136d7
github username apalski <github email address annette.drapalski@gmail.com>
2018-05-24 20:07:36 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+	drivers.push(name);
+}	
+
+function destructivelyPrependDriver(name) {
+	drivers.unshift('Bob');
+}	
+
+function destructivelyRemoveLastDriver() {
+	drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+	drivers.shift();
+}
+
+function appendDriver(name) {
+	return [...drivers, name];
+}	
+
+function prependDriver(name) {
+	return ['Arnold', ...drivers];
+}
+
+function removeLastDriver() {	
+	return drivers.slice(0, -1);
+}	
+
+function removeFirstDriver() {
+	return drivers.slice(1);
+}	
