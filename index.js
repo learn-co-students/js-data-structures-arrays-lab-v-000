4741ad2e44771d2b943b273d67aaa701e0e0f982
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-02 04:34:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+    return drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift()
+}
+
+function appendDriver(name) {
+    const nu_drivers = [ ...drivers, name ]
+    return nu_drivers
+}
+
+function prependDriver(name) {
+    const nu_drivers = [ name, ...drivers ]
+    return nu_drivers
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
