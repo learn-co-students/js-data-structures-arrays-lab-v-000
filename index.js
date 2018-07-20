6c803776863cf159bd2b8c2af68b16afb49c96fa
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-20 10:12:12 -0700

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name){
+    return drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name){
+    return drivers.shift(name);
+}
+
+function appendDriver(name){
+   let newDrivers = [ ...drivers, name];
+    return newDrivers;
+}
+
+function prependDriver(name){
+     let newDrivers = [name, ...drivers];
+     return newDrivers;
+}
+
+function removeLastDriver(name){
+    let newDrivers = drivers.slice(0,2);
+    return newDrivers;
+}
+
+function removeFirstDriver(name) {
+    let newDrivers = drivers.slice(1)
+    return newDrivers;
+}
+//push unshift pop shift
