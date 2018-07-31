1dc4433c334e6365347f20cf8d96cba9099c8bfb
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-07-31 14:23:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name)
+}
+
+
+function appendDriver(name){
+  let newDivers = [...drivers, name]
+  return newDivers
+}
+
+function prependDriver(name){
+  let newDriver = [name, ...drivers]
+  return newDriver
+}
+
+
+function removeLastDriver(){
+  let lastDriver = drivers.slice(0,2)
+  return lastDriver
+}
+
+function removeFirstDriver(){
+  let firstDriver = drivers.slice(1)
+  return firstDriver
+}
