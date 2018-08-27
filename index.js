4a54e4eadf3afa68a15a41b8f56607d274ef7d32
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-27 04:50:18 +0000

    Done.

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  return [ ...drivers.slice(), name];
+  return [ ...drivers, name];
 }
 function prependDriver(name) {
-  return [ name, ...drivers.slice()];
+  return [ name, ...drivers];
 }
 function removeLastDriver() {
   return drivers.slice(0,drivers.length-1);
