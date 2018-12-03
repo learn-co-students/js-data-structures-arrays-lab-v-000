29c3c673ce165c4d7954c762d59473d91fbf967f
epic0448 <samuelogundiran@Yahoo.com>
2018-12-03 10:15:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+ const drivers = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendDriver() {
+   drivers.push('Ralph');
+ }
+
+ function destructivelyPrependDriver(){
+     drivers.unshift('Bob');
+ }
+
+ function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+
+}
+
+function destructivelyRemoveLastDriver(){
+    const drivers = ["Milo", "Otis", "Garfield"];
+    drivers.pop();
+}
+
+
