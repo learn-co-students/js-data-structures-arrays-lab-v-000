367b77ccef69710a36afd51cb1dbedfba8cf4f71
epic0448 <samuelogundiran@Yahoo.com>
2018-12-03 10:53:25 -0500

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,38 @@
      drivers.unshift('Bob');
  }
 
- function destructivelyRemoveFirstDriver(){
-    drivers.shift();
-
-}
-
-function destructivelyRemoveLastDriver(){
-    const drivers = ["Milo", "Otis", "Garfield"];
+ function destructivelyRemoveLastDriver(){
     drivers.pop();
 }
+ function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+// >>>>>>> EVERY code Below Here Is not DESTRUCTIVE<<<<<<<
+
+function appendDriver(){
+    const newArray = [...drivers, 'Broom'];
+    return newArray;
+}
+
+function prependDriver(){
+    const newArray= ['Arnold', ...drivers];
+    return newArray;
+}
+
+function removeLastDriver(){
+    const newArray = drivers.slice(0, drivers.length - 1);
+    return newArray;
+}
+
+function removeFirstDriver(){
+    const newArray = drivers.slice(1);
+    return newArray;
+}
+
+
+
+
+
 
 
