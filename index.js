9c52c34cd170b8c98fd20c18b04e9fcfb15a0c79
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2018-12-17 13:50:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+
+
+  const drivers = ['Milo', 'Otis', 'Garfield']
+
+  function destructivelyAppendDriver(d){
+    drivers.push(d)
+  }
+
+  function destructivelyPrependDriver(d){
+    drivers.unshift(d)
+  }
+
+  function destructivelyRemoveLastDriver(){
+    drivers.pop()
+  }
+
+  function destructivelyRemoveFirstDriver(){
+    drivers.shift()
+  }
+
+  function appendDriver(name){
+    newDrivers = [...drivers.slice(0),name]
+    return newDrivers
+  }
+
+  function prependDriver(name){
+    newDrivers = [name,...drivers.slice(0)]
+    return newDrivers
+  }
+
+  function removeLastDriver(){
+    newDrivers = drivers.slice(0, drivers.length - 1)
+    return newDrivers
+  }
+
+  function removeFirstDriver(){
+    newDrivers = drivers.slice(1)
+    return newDrivers
+  }
