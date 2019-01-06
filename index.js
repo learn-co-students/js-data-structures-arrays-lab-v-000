06c21082245855f13e76a002d6957a56a8c08eea
Joanna Sese <jmgsese@gmail.com>
2019-01-05 20:55:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const drivers = ['Milo', 'Otis', 'Garfield'];
-destructivelyAppendDriver = () => drivers.push('Ralph');
-destructivelyPrependDriver = () => drivers.unshift('Bob');
+destructivelyAppendDriver = (name) => drivers.push(name);
+destructivelyPrependDriver = (name) => drivers.unshift(name);
 destructivelyRemoveLastDriver = () => drivers.pop();
 destructivelyRemoveFirstDriver = () => drivers.shift();
-appendDriver = () => drivers.concat('Broom');
-prependDriver = () => ['Arnold', ...drivers];
+appendDriver = (name) => drivers.concat(name);
+prependDriver = (name) => [name, ...drivers];
 removeLastDriver = () => drivers.slice(0, drivers.length-1);
-removeFirstDriver = () => drivers.slice(1, drivers.length);
+removeFirstDriver = () => drivers.slice(1);
