1ef49ea8aade144dc77e4bd38dc34277c10e4abb
David McGrann <dpmcgrann@gmail.com>
2019-01-15 18:27:09 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const drivers = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendDriver(name) {
-  drivers.push("Ralph");
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name) {
-  drivers.unshift("Bob");
+  drivers.unshift(name);
 }
 
 function destructivelyRemoveLastDriver() {
@@ -18,22 +18,18 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  name = [...drivers, "Broom"];
-  return name;
+  return [...drivers, name];
 }
 
 function prependDriver(name) {
-  name = ["Arnold", ...drivers];
-  return name;
+  return [name, ...drivers];
 }
 
 function removeLastDriver() {
-  copyOfDrivers = drivers.slice(0, drivers.length - 1);
-  return copyOfDrivers;
+  return drivers.slice(0, drivers.length - 1);
 }
 
 function removeFirstDriver() {
-  copyOfDrivers = drivers.slice(1);
-  return copyOfDrivers;
+  return drivers.slice(1);
 }
 
