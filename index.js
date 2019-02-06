5ef4b57ef321ed6aedb0bee3965a0e75c54c7ae3
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-06 03:54:40 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ function destructivelyAppendDriver() {
 function destructivelyPrependDriver() {
   drivers.unshift("Bob");
 }
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const appendedDrivers = [...drivers, name];
+  return appendedDrivers;
+}
+
+function prependDriver(name) {
+  const prependedDrivers = [name, ...drivers];
+  return prependedDrivers;
+}
+
+function removeLastDriver() {
+  const lastDriverRemoved = drivers.slice(0, 2);
+  return lastDriverRemoved;
+}
+
+function removeFirstDriver() {
+  const firstDriverRemoved = drivers.slice(-2);
+  return firstDriverRemoved;
+}
