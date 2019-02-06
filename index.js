323721b8f7624124f3fe2b41b31915ec9becdf70
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-06 03:42:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver() {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift("Bob");
+}
