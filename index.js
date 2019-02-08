64bf42db8a9ec15a731cc277af97c619fd55bc81
Christine Cua <cloudycua@gmail.com>
2019-02-08 20:29:27 +0000

    Done.

--- index.js
+++ index.js
@@ -17,5 +17,17 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(name) {
-  drivers.splice(drivers.length - 1, 0, name);
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
 }
