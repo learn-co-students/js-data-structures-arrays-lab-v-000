94ffbc09e9adc1ef371b8dfa8bb242032d274ad9
github username <github email address>
2019-03-05 22:47:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver() {
+  drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver() {
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+  drivers;
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+  drivers;
+}
+
+function appendDriver(name) {
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, drivers.length - 1);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
