4905aa0e6a4488310a5f862c59936ccff8b66500
AlwinaO <aoyewole2586@gmail.com>
2019-09-06 22:25:25 +0100

    Done.

--- index.js
+++ index.js
@@ -26,7 +26,17 @@ let drivers = ["Milo", "Otis", "Garfield"];
  }
 
  function appendDriver(name) {
-     const newDrivers = [...drivers];
+     return [...drivers, name];
+ }
 
-     newDrivers;
+ function prependDriver(name) {
+   return [name, ...drivers];
+ }
+
+ function removeLastDriver() {
+     return drivers.slice(0, drivers.length - 1);
+ }
+
+ function removeFirstDriver() {
+   return drivers.slice(1);
  }
